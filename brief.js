70be12d750ed33aae1c60cdc52f5b004bcbbcd5f
Evgenii Klimov <disturh@gmail.com>
2018-04-06 20:49:09 +0300

    fixes according to ESlint

--- brief.js
+++ brief.js
@@ -1,36 +1,35 @@
 module.exports = function go(flow, errCb) {
-  const isFunction = (test) => typeof test === 'function';
-  const isArray = (test) => test instanceof Array;
-  const lastOf = (array) => array[ array.length-1 ];
-  const pushNew = (levels) => levels.push({ functions: [] });
+  const isFunction = test => typeof test === 'function';
+  const isArray = test => test instanceof Array;
+  const lastOf = array => array[array.length - 1];
+  const pushNew = levels => levels.push({ functions: [] });
 
 
-  let steps = [];
-  flow.forEach(el => {
+  const stepsArr = [];
+  flow.forEach((el) => {
     if (isArray(el)) {
-      pushNew(steps);
-      lastOf(steps).args = el.concat();
-    }
-    else if (isFunction(el)) {
-      lastOf(steps).functions.push(el);
+      pushNew(stepsArr);
+      lastOf(stepsArr).args = el.concat();
+    } else if (isFunction(el)) {
+      lastOf(stepsArr).functions.push(el);
     }
   });
 
-  run(steps, 0);
-  function run(steps, stepIndex) { // recursive
+
+  (function run(steps, stepIndex) { // recursive
     if (stepIndex === steps.length) return;
-    const lastStep = stepIndex === steps.length-1 ? true : false;
+    const lastStep = (stepIndex === steps.length - 1);
     const step = steps[stepIndex];
 
     if (step.prevStepResults) {
       // console.log(step.prevStepResults);
-      step.functions.forEach(func => {
+      step.functions.forEach((func) => {
         if (!isFunction(func)) return;
 
         let nextStepArgIndex;
-        const nextStepIndex = steps.findIndex(nextStep => {
+        const nextStepIndex = steps.findIndex((nextStep) => {
           nextStepArgIndex = nextStep.args.findIndex(arg => arg === func);
-          if (nextStepArgIndex !== -1) return true;
+          return (nextStepArgIndex !== -1);
         });
         const arg = step.prevStepResults.length >= 2 ?
           step.prevStepResults :
@@ -41,19 +40,19 @@ module.exports = function go(flow, errCb) {
           steps[nextStepIndex].args[nextStepArgIndex] = new Promise(func.bind(null, arg));
         }
         if (lastStep) {
-          func(arg, ()=>{}, ()=>{});
+          func(arg, () => {}, () => {});
         }
       });
 
-      run(steps, ++stepIndex);
+      run(steps, stepIndex + 1);
     } else {
-      Promise.all(step.args).then(res => {
+      Promise.all(step.args).then((res) => {
         step.prevStepResults = res;
         run(steps, stepIndex);
       })
-      .catch(err => errCb && errCb());
+        .catch(err => errCb && errCb(err));
     }
-  }
+  }(stepsArr, 0));
 
-  return steps;
-}
+  return stepsArr;
+};
